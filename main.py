f048d209d75241610a7e005f41b04bc104221c08
Yossi Cohen <metalyos@gmail.com>
2019-05-03 23:06:32 +0300

    Added VerticalScrolledFrame

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 from tkinter import *
+from tkinter import ttk
 import os
 from subprocess import call
+
+
+class VerticalScrolledFrame(Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+    """
+
+    def __init__(self, parent, *args, **kw):
+        Frame.__init__(self, parent, *args, **kw)
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        def _on_mousewheel(event):
+            canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+        self.interior.bind_all("<MouseWheel>", _on_mousewheel)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
 
 
 class AppEntryFrame(Frame):
@@ -99,6 +149,9 @@ class MainApp(Tk):
         self.exit_app_button.pack(side=RIGHT, padx=20, pady=20)
 
     def init_ui_from_file(self):
+        self.listview_frame = VerticalScrolledFrame(self)
+        self.listview_frame.pack(side=TOP, expand=YES, fill=BOTH)
+
         name = None
         pack_install = None
         description = None
@@ -110,7 +163,7 @@ class MainApp(Tk):
                     description = app_file.readline().rstrip()
 
                     entry = AppEntryFrame(
-                        self, name, pack_install, description)
+                        self.listview_frame.interior, name, pack_install, description)
                     entry.configure(bd=3, relief=GROOVE)
                     entry.pack(side=TOP, padx=10, pady=5, expand=YES, fill=X)
                     self.entries.append(entry)
@@ -139,7 +192,7 @@ class MainApp(Tk):
 
 def main():
     top = MainApp()
-    top.minsize(500, 100)
+    top.minsize(500, 500)
     top.mainloop()
 
 
